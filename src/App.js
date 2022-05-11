613d552a9b5183ab4ec73e4a439df4299e792255
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-05-11 22:11:56 +0200

    Prepare cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Hero from './components/Hero/Hero'
 import SearchForm from './components/SearchForm/SearchForm'
 import List from './components/List/List'
 import Container from './components/Container/Container';
+import CardForm from './components/CardForm/CardForm';
 
 const App = () => {
   return (
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,13 @@
 import styles from './Column.module.scss';
+import CardForm from '../CardForm/CardForm'
 
 const Column = props => {
     return <article className={styles.column}>
     <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h2>
+    <ul className={styles.cards}>
+        {props.cards.map(card => <li key={card.id}>{card.title}</li>)}
+    </ul>
+    <CardForm columnId={props.id} action={props.action} />
     </article>
 }
  
