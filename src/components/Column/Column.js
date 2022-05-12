a3602e88d9f1ab2f18e33744a825e49cdbbd70a4
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-05-12 08:56:03 +0200

    Fix card view

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,11 +1,12 @@
 import styles from './Column.module.scss';
 import CardForm from '../CardForm/CardForm'
+import Card from '../Card/Card';
 
 const Column = props => {
     return <article className={styles.column}>
     <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h2>
     <ul className={styles.cards}>
-        {props.cards.map(card => <li key={card.id}>{card.title}</li>)}
+        {props.cards.map(card => <Card key={card.id} title={card.title} />)}
     </ul>
     <CardForm columnId={props.id} action={props.action} />
     </article>
