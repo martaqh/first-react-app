a98817ca7fd43808d0f5ada0e930c24006dc9697
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-05-24 15:16:02 +0200

    Add ADD_COLUMN and ADD_CARD actions to the reducer

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,14 +1,17 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 import styles from './CardForm.module.scss';
 
 
 const CardForm = props => {
+    const dispatch = useDispatch();
     const [title, setTitle] = useState('');
+    const columnId = props.columnId;
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title }, props.columnId);
+        dispatch({ type: 'ADD_CARD', payload: {title, columnId} });
         setTitle('');
     }
 
