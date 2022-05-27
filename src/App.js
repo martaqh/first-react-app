5dec3d47e0b96d1b7870b22f06b0982007ca0b8c
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-05-27 16:01:26 +0200

    Add NoMatch component for 404 handling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Favorite from './components/Favorite/Favorite';
+import NoMatch from './components/NoMatch/NoMatch';
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -15,6 +16,7 @@ const App = () => {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/favorite" element={<Favorite />} />
+              <Route path="*" element={<NoMatch />} />
             </Routes>
           </Container>
       </main>
---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,10 @@
+import PageTitle from "../PageTitle/PageTitle"
+
+
+const NoMatch = () => {
+    return (
+        <PageTitle>404 Not Found</PageTitle>
+    )
+}
+
+export default NoMatch
