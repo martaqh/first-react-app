f7689185091af537a07bebe20500f778889aeecc
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-05-27 15:38:18 +0200

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import Hero from './components/Hero/Hero'
-import SearchForm from './components/SearchForm/SearchForm'
-import List from './components/List/List'
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Favorite from './components/Favorite/Favorite';
 
 const App = () => {
   return (
       <main>
         <NavBar />
-        <Container>
-        <Hero />
-        <SearchForm />
-        <List />
-      </Container>
+          <Container>
+            <Home />
+            <About />
+            <Favorite />
+          </Container>
       </main>
       
   );
--- src/components/PageTitle/PageTitle.js
+++ src/components/PageTitle/PageTitle.js
@@ -2,7 +2,7 @@ import styles from './PageTitle.module.scss';
 
 const PageTitle = props => {
     return (
-        <h2 className={styles.title}>{props.children}</h1>
+        <h2 className={styles.title}>{props.children}</h2>
     )
 }
 
