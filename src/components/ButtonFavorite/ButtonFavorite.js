2553132256cc50ca86e014748cc47b6a77bdfe81
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-05-29 15:28:25 +0200

    Add ButtonFavorite component and toggle isFavorite property

---
+++ src/components/ButtonFavorite/ButtonFavorite.js
@@ -0,0 +1,20 @@
+import styles from './ButtonFavorite.module.scss';
+import clsx from 'clsx';
+import { useDispatch } from 'react-redux';
+import { toggleCardFavorite } from '../../redux/store';
+
+
+const ButtonFavorite = props => {
+    const dispatch = useDispatch();
+    const cardId = props.id;
+    console.log(props);
+    const handleClick = e => {
+        e.preventDefault();
+        console.log('clicked');
+        dispatch(toggleCardFavorite(cardId));
+    }
+    
+    return <button className={clsx(styles.buttonFavorite, props.isFavorite === true && styles.active)} onClick={handleClick}><span className='fa fa-star-o'></span></button>
+}
+
+export default ButtonFavorite
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,12 @@
 import styles from './Card.module.scss';
+import ButtonFavorite from '../ButtonFavorite/ButtonFavorite';
+import { useSelector } from 'react-redux';
 
 const Card = props => {
-    return <li className={styles.card}>{props.title}</li>
+    console.log(props);
+    return (
+        <li className={styles.card}>{props.title}<ButtonFavorite isFavorite={props.isFavorite} id={props.id} /></li>
+    )
 }
 
 export default Card;
